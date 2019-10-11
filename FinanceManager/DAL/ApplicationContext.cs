c6ef8299126600521517f728f23392ac691f557b
Michael Frye <13396299+MikeSemicolonD@users.noreply.github.com>
2019-10-10 22:28:37 -0400

    Start of a DAL

---
+++ FinanceManager/DAL/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using FinanceManager.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace FinanceManager.DAL
+{
+    public class ApplicationContext
+    {
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+        {
+            public ApplicationDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+        }
+    }
+}
---
+++ FinanceManager/DAL/TestQuery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace FinanceManager.DAL
+{
+    public class TestQuery
+    {
+        public void TestSelect()
+        {
+        //https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand?view=netframework-4.8
+        //    SqlConnection dp = new SqlConnection();
+        //    string query = "USE UserInfo GO SELECT name, database_id, create_dateFROM sys.databases; GO";
+        //    using (DbConnection conn = dp.GetConnection())
+        //    {
+        //        try
+        //        {
+        //            SqlCommand command = dp.CreateCommand();
+        //            conn.Open();
+        //            cmd.ExecuteNonQuery();
+        //            conn.Close();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            throw ex;
+        //        }
+        //        finally
+        //        {
+        //            if (conn.State == ConnectionState.Open)
+        //            {
+        //                conn.Close();
+        //            }
+        //        }
+        //    }
+        }
+    }
+}
